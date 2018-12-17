e813ae3aa1fc4d05f303d478b81f7fbf48e2b24d
ayoub <fadadi.ayoub@gmail.com>
2018-12-17 15:59:21 +0100

    Setting Up Angular And Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileuploaderComponent } from './fileuploader/fileuploader.component';
+import { HeaderComponent } from './header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileuploaderComponent,
+    HeaderComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
