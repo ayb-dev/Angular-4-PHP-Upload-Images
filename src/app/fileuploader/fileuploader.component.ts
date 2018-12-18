a2da45f5eb33794797bdbcc32cdf81e86268eca4
ayoub <fadadi.ayoub@gmail.com>
2018-12-18 16:28:04 +0100

    Managing Uploaded Images With Uploadcare

--- src/app/fileuploader/fileuploader.component.ts
+++ src/app/fileuploader/fileuploader.component.ts
@@ -14,13 +14,14 @@ export class FileuploaderComponent implements OnInit {
   }
 
   uploadFile(event){
+    console.log('1');
     let elem = event.target;
     if(elem.files.length>0){
       console.log(elem.files[0]);
       let formData = new FormData();
       formData.append('file', elem.files[0]);
 
-      this.http.post('http://localhost/script.php', formData).subscribe((data) => {
+      this.http.post('http://localhost/angular-php/script.php', formData).subscribe((data) => {
         console.log('Got some data from backend ', data);
       }, (error) => {
         console.log('Error ', error);
